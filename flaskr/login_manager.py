1b035f9f7850031b6c72f5f0caa06b17ba63f79e
James Rodriguez <james.rodriguez5@techexchange.in>
2023-02-28 03:04:48 +0000

    login manager was not needed it was actually user class

--- flaskr/login_manager.py
+++
@@ -1,5 +0,0 @@
-# Ignore red underline for the flask_login
-# If you can import it in the using python repl on the console it is fine.
-
-from flask_login import LoginManager
-login_manager = LoginManager()
