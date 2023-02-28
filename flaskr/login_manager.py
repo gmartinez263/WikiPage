6df7b9c9e893f28fd240cf550777c4580bd4ea24
James Rodriguez <james.rodriguez5@techexchange.in>
2023-02-28 02:49:34 +0000

    made module for login manager

--- flaskr/login_manager.py
+++ flaskr/login_manager.py
@@ -1,2 +1,5 @@
+# Ignore red underline for the flask_login
+# If you can import it in the using python repl on the console it is fine.
+
 from flask_login import LoginManager
 login_manager = LoginManager()
