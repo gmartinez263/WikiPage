6a72b578e1da1d61eb936c0cf87886997eefeecc
Geovanny <gmartinez@techexchange.in>
2023-03-03 09:31:18 +0000

    added methods for each html file and added a method to get the image of each of the authors

--- flaskr/pages.py
+++ flaskr/pages.py
@@ -1,14 +1,41 @@
 from flask import render_template
+from flask_login import LoginManager
 
 
 def make_endpoints(app):
 
-    # Flask uses the "app.route" decorator to call methods when users
-    # go to a specific route on the project's website.
     @app.route("/")
     def home():
-        # TODO(Checkpoint Requirement 2 of 3): Change this to use render_template
-        # to render main.html on the home page.
-        return render_template("main.html")
+        return render_template("home.html")
 
-    # TODO(Project 1): Implement additional routes according to the project requirements.
+    @app.route("/pages")
+    def pages():
+        return render_template("pages.html")
+
+    @app.route("/about")
+    def about():
+        return render_template("about.html", authors = ["James", "Ale", "Geovanny"])
+
+    @app.route("/login", methods = ["POST", "GET"])
+    def login():
+        return render_template("login.html")
+
+    @app.route("/signup", methods = ["POST", "GET"])
+    def signup():
+        return render_template("signup.html")
+
+    @app.route("/images/<img_name>")
+    def get_author_images(img_name):
+        from google.cloud import storage
+        from flask import Response
+        
+        # Instantiates a client
+        storage_client = storage.Client()
+        # Bucket names
+        content_wiki_name = "content-wiki"
+        # bucket instanttiation
+        content_wiki_bucket = storage_client.bucket(content_wiki_name)
+        # Hopefully getting the image
+        image_blob = content_wiki_bucket.blob(f"images/{img_name}")
+        image = image_blob.download_as_bytes()
+        return Response(image)
